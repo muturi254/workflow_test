8ae67d6f3963260245a7a95b4db68f8eb8be17e6
muturi254 <petermuturi74@gmail.com>
2023-09-27 11:55:54 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste aspernatur excepturi, quae ea officia quibusdam. Quidem modi expedita cupiditate, numquam quis magni odio? Itaque eos cupiditate ducimus culpa corrupti accusantium?</p>
+    </>
+  )
+}
+
+export default App
